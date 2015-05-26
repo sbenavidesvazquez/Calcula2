8e5705bb0ca89c4fdf9bf161ee170f447c39bb95
sbenavidesvazquez <sbenavidesvazquez@atanasoft15.danielcastelao.org>
2015-05-26 12:03:17 +0200

    calculadora testeada

--- test/calculadora2/Calculadora2Test.java
+++ test/calculadora2/Calculadora2Test.java
@@ -45,15 +45,39 @@ public class Calculadora2Test {
      */
     @Test
     public void testRealizaOperacion() {
-        System.out.println("realizaOperacion");
+        System.out.println("Suma");
         float n1 = 5.0F;
         float n2 = 2.0F;
-        String op = "*";
-        float expResult = 10.0F;
+        String op = "+";
+        float expResult = 7.0F;
         float result = Calculadora2.realizaOperacion(n1, n2, op);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
+        
+        System.out.println("Resta");
+        float n3=7.0F;
+        float n4=4.0F;
+        String op2="-";
+        float expResult2=3.0F;
+        float result2 = Calculadora2.realizaOperacion(n3, n4, op2);
+        assertEquals(expResult2, result2, 0.0);
+        
+        System.out.println("Multiplicar");
+        float n5=3.0F;
+        float n6=10.0F;
+        String op3="*";
+        float expResult3=30.0F;
+        float result3= Calculadora2.realizaOperacion(n5, n6, op3);
+        assertEquals(expResult3, result3, 0.0);
+        
+        System.out.println("Dividir");
+        float n7=4.0F;
+        float n8=2.0F;
+        String op4="/";
+        float expResult4=2.0F;
+        float result4 = Calculadora2.realizaOperacion(n7, n8, op4);
+        assertEquals(expResult4, result4, 0.0);
     }
     
 }
